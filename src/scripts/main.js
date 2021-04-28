23accdd78a354ded1415baa20cf9e7ab44c93e72
Ping <4ivrik@ukr.net>
2021-04-28 16:58:05 +0300

    finished test-task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,9 +13,9 @@ const swiper = new Swiper('.swiper-container', {
 
 function burgerMenu(selector) {
   const menu = $(selector);
-  const button = menu.find('.burger-menu_button', '.burger-menu_lines');
-  const links = menu.find('.burger-menu_link');
-  const overlay = menu.find('.burger-menu_overlay');
+  const button = menu.find('.burger-menu__button', '.burger-menu__lines');
+  const links = menu.find('.burger-menu__link');
+  const overlay = menu.find('.burger-menu__overlay');
 
   button.on('click', (e) => {
     e.preventDefault();
@@ -26,8 +26,8 @@ function burgerMenu(selector) {
   overlay.on('click', () => toggleMenu());
 
   function toggleMenu() {
-    menu.toggleClass('burger-menu_active');
-    if (menu.hasClass('burger-menu_active')) {
+    menu.toggleClass('burger-menu__active');
+    if (menu.hasClass('burger-menu__active')) {
       $('body').scss('overlow', 'hidden');
     } else {
       $('body').scss('overlow', 'visible');
